90b71cb786a715cd7ed61881d36363507dd9b1d0
Yannick Loriot <yannick.loriot@gmail.com>
2015-10-29 15:26:38 +0100

    [ADD] Framework for carthage

---
+++ Example/YLTCPBroadcasterFramework/YLTCPBroadcasterFramework.h
@@ -0,0 +1,19 @@
+//
+//  YLTCPBroadcasterFramework.h
+//  YLTCPBroadcasterFramework
+//
+//  Created by Yannick LORIOT on 29/10/15.
+//  Copyright © 2015 Yannick Loriot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLTCPBroadcasterFramework.
+FOUNDATION_EXPORT double YLTCPBroadcasterFrameworkVersionNumber;
+
+//! Project version string for YLTCPBroadcasterFramework.
+FOUNDATION_EXPORT const unsigned char YLTCPBroadcasterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLTCPBroadcasterFramework/PublicHeader.h>
+
+#import <YLTCPBroadcasterFramework/YLTCPBroadcaster.h>
